20211ec13e460ab0233deba8977907e80dd98223
ayman <aymanmohamed1142001@gmail.com>
2026-08-20 20:00:54 +0300

    add asset image and sound and create tokuappbody widget

---
+++ lib/widgets/container_item.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+class container extends StatelessWidget {
+  const container({
+    super.key, required this.color, required this.text,
+  });
+   
+   final Color color;
+   final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: 90,
+      width: double.infinity,
+      color: color,
+      child: Text(text,style: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+      ),),
+    );
+  }
+}
---
+++ lib/widgets/toku_app_body.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:lnguage_app/widgets/container_item.dart';
+
+class TokuAppBody extends StatelessWidget {
+  const TokuAppBody ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        container(
+          color:Color(0xffEf9235),
+          text: 'Numbers',
+        ),
+
+       
+
+      ],
+    );
+  }
+}
